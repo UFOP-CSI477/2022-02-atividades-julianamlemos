80e9e8c37c8f259c566f5684625a905c186957fb
Juliana M. Lemos <42423350+julianamlemos@users.noreply.github.com>
2023-03-23 17:02:38 -0300

    Create server.js

---
+++ Atividades/exercicios/007-aplicacao/server/src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+
+
+import { mainRouter } from './routes/main.js';
+import { estadoRouter } from './routes/estado.js';
+
+const PORT = 3333;
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+// Routes:
+app.use(mainRouter);
+app.use(estadoRouter);
+
+
+// Server - start/listen
+app.listen(PORT, () => {
+
+    console.log(`[SERVER] Server is running on port ${PORT}`);
+
+});
