3275f2637b3e4b2d5ad7e4e97752b1088347e34d
Juliana M. Lemos <42423350+julianamlemos@users.noreply.github.com>
2023-03-23 17:21:00 -0300

    Create Menu.tsx

---
+++ Atividades/exercicios/007-aplicacao/web-vite/src/components/menu/Menu.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import './menu.css'
+
+const Menu = () => {
+
+  
+
+    return(
+        <div>
+            <h2>Menu</h2>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/estados'>Listar Estados</Link></li>
+                <li><Link to='/estados/create'>Inserir Estado</Link></li>
+
+            </ul>
+        </div>
+    );
+
+}
+
+export default Menu;
