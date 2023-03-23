2ea45dc184168903c012c7075a47bd507bd1e49e
Juliana M. Lemos <42423350+julianamlemos@users.noreply.github.com>
2023-03-23 16:57:12 -0300

    Create server.js

---
+++ Atividades/exercicios/006-nodejs/introducao/src/server.js
@@ -0,0 +1,9 @@
+// Import
+import app, { config } from './routes.js';
+
+// Rotas -- express.js
+
+// API Server ----
+ app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}...`);
+});
